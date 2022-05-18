46db57eade012cbdfe8c2eec16238e27d41dacce
Alex Morton <alexmorton_dev@protonmail.com>
2022-05-18 22:22:32 +0300

    start refactoring links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import macbook from '../styles/img/mac.avif';
+import macbookpng from '../styles/img/macb.png';
+import macHqPng from '../styles/img/mac-hq.png';
+import macHqAvif from '../styles/img/mac-hq-avif.avif';
+import macHqLow from '../styles/img/mac-hq-low.avif';
 
-const Home = () => <div>Home Page</div>;
+const Home = () => (
+        <div>
+                <img src={macbook} alt="macbook" />
+                <img src={macbookpng} alt="macbook" />
+                <img src={macHqPng} alt="macbook" />
+                <img src={macHqAvif} alt="macbook" />
+                <img src={macHqLow} alt="macbook" />
+        </div>
+);
 
 export default Home;
