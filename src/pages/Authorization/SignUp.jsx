c58649db32d6bc57d10bc280b54f72ba94f53d25
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-01 15:54:34 +0300

    finish auth components

--- src/pages/Authorization/SignUp.jsx
+++ src/pages/Authorization/SignUp.jsx
@@ -1,12 +1,17 @@
-import { useEffect } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { FiEyeOff, FiEye } from 'react-icons/fi';
 import { Container } from '../../styles/Container';
 import { TextErrorSm, TextNormalSm, LinkSmMd, HeadingSmSbBase, SubHeadTextMdNorm } from '../../styles/UI/Text';
-import { Wrapper, LoginBlock, LoginText, LoginForm, LoginBtn, LoginIcon } from './Login.styled';
+import { LoginText, LoginBtn, LoginIcon } from './Login.styled';
 import googleIcon from '../../styles/img/auth/google.svg';
-import { SignUpForm, SignUpBlock, InputBlock } from './SignUp.styled';
+import { SignUpForm, SignUpBlock, InputBlock, ErrorBlock, Wrapper, TextSeparator } from './SignUp.styled';
+import { useToggle } from '../../hooks/useToggle';
 
 const SignUp = () => {
+        const [isVisible, setIsVisible] = useToggle();
+
         const {
                 register,
                 formState: { errors, isValid },
@@ -18,17 +23,34 @@ const SignUp = () => {
                 console.log(JSON.stringify(data));
                 reset();
         };
+        const toggleVisible = () => {
+                setIsVisible((prev) => !prev);
+        };
 
         useEffect(() => {
                 window.scrollTo(0, 0);
         }, []);
+
+        //  === Error Styling ===
+        const errorUser = errors?.userName && {
+                marginBottom: '0',
+                border: '1px solid var(--error-500)',
+        };
+        const errorEmail = errors?.email && {
+                marginBottom: '0',
+                border: '1px solid var(--error-500)',
+        };
+        const errorPasword = errors?.password && {
+                marginBottom: '0',
+                border: '1px solid var(--error-500)',
+        };
 
         return (
                 <Wrapper>
                         <Container>
                                 <SignUpBlock>
                                         <LoginText>
-                                                <h1>🔐</h1>
+                                                <h1>👋</h1>
                                                 <HeadingSmSbBase>Create an account</HeadingSmSbBase>
                                                 <SubHeadTextMdNorm style={{ margin: '1rem 0 3rem 0' }}>
                                                         Start your 30-day free trial.
@@ -36,60 +58,99 @@ const SignUp = () => {
                                         </LoginText>
 
                                         <SignUpForm onSubmit={handleSubmit(onSubmit)}>
-                                                <label htmlFor="userName">User Name</label>
-                                                <InputBlock>
+                                                {/* USERNAME */}
+                                                <label htmlFor="userName">Name</label>
+                                                <InputBlock style={errorUser}>
                                                         <input
                                                                 placeholder="Enter your user name"
                                                                 {...register('userName', {
                                                                         required: 'User name is required',
                                                                         pattern: {
-                                                                                value: /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-                                                                                message: 'User name must be between 8 and 20 characters long, and can only contain letters, numbers, and the underscore',
+                                                                                value: /^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+                                                                                message: 'User name must be between 4 and 20 characters long, and can only contain letters, numbers, and the underscore',
                                                                         },
                                                                 })}
                                                         />
                                                 </InputBlock>
-
                                                 {/* ERROR MESSAGE */}
-                                                {errors?.userName && (
-                                                        <TextErrorSm>
-                                                                {errors?.userName?.message ||
-                                                                        'Hm... something went wrong'}
-                                                        </TextErrorSm>
-                                                )}
+                                                <ErrorBlock>
+                                                        {errors?.userName && (
+                                                                <TextErrorSm>
+                                                                        {errors?.userName?.message ||
+                                                                                'Hm... something went wrong'}
+                                                                </TextErrorSm>
+                                                        )}
+                                                </ErrorBlock>
 
-                                                <label htmlFor="password">Password</label>
-
-                                                <InputBlock>
+                                                {/* EMAIL */}
+                                                <label htmlFor="email">E-mail</label>
+                                                <InputBlock style={errorEmail}>
                                                         <input
-                                                                type="password"
-                                                                id="password"
-                                                                placeholder="••••••••"
-                                                                {...register('password', {
-                                                                        required: 'Password is required',
+                                                                placeholder="Enter your e-mail"
+                                                                {...register('email', {
+                                                                        required: 'User name is required',
+                                                                        pattern: {
+                                                                                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                                                                message: 'Invalid email address',
+                                                                        },
                                                                 })}
                                                         />
                                                 </InputBlock>
-
                                                 {/* ERROR MESSAGE */}
-                                                {errors?.password && (
-                                                        <TextErrorSm>
-                                                                {errors?.password?.message ||
-                                                                        'Hm... something went wrong'}
-                                                        </TextErrorSm>
-                                                )}
+                                                <ErrorBlock>
+                                                        {errors?.email && (
+                                                                <TextErrorSm>
+                                                                        {errors?.email?.message ||
+                                                                                'Hm... something went wrong'}
+                                                                </TextErrorSm>
+                                                        )}
+                                                </ErrorBlock>
 
-                                                <LoginBtn type="submit">Sign In</LoginBtn>
+                                                {/* PASSWORD */}
+                                                <label htmlFor="password">Password</label>
+                                                <InputBlock style={errorPasword}>
+                                                        <input
+                                                                type={isVisible ? 'text' : 'password'}
+                                                                placeholder="••••••••"
+                                                                {...register('password', {
+                                                                        required: 'Password is required',
+                                                                        pattern: {
+                                                                                value: /^(?=.{10,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/,
+                                                                                message: 'A password containing at least 1 uppercase, 1 lowercase, 1 digit, 1 special character and have a length of at least of 10',
+                                                                        },
+                                                                })}
+                                                        />
+
+                                                        <i>
+                                                                {isVisible ? (
+                                                                        <FiEyeOff onClick={toggleVisible} />
+                                                                ) : (
+                                                                        <FiEye onClick={toggleVisible} />
+                                                                )}
+                                                        </i>
+                                                </InputBlock>
+                                                {/* ERROR MESSAGE */}
+                                                <ErrorBlock>
+                                                        {errors?.password && (
+                                                                <TextErrorSm>
+                                                                        {errors?.password?.message ||
+                                                                                'Hm... something went wrong'}
+                                                                </TextErrorSm>
+                                                        )}
+                                                </ErrorBlock>
+
+                                                {/* BUTTONS */}
+                                                <LoginBtn type="submit">Sign Up</LoginBtn>
 
                                                 <LoginIcon type="button">
-                                                        <img src={googleIcon} alt="google-icon" /> Sign in with Google
+                                                        <img src={googleIcon} alt="google-icon" /> Sign up with Google
                                                 </LoginIcon>
 
-                                                <div>
-                                                        <TextNormalSm>Don’t have an account?</TextNormalSm>
+                                                <TextSeparator>
+                                                        <TextNormalSm>Have an account?</TextNormalSm>
 
-                                                        <LinkSmMd to="/signup">Sign Up</LinkSmMd>
-                                                </div>
+                                                        <LinkSmMd to="/login">Log In</LinkSmMd>
+                                                </TextSeparator>
                                         </SignUpForm>
                                 </SignUpBlock>
                         </Container>
--- src/pages/Authorization/SignUp.styled.jsx
+++ src/pages/Authorization/SignUp.styled.jsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.section`
+        margin: 2rem auto;
+        height: 100vh;
+`;
+
 export const SignUpForm = styled.form`
         display: flex;
         justify-content: center;
@@ -23,11 +28,11 @@ export const SignUpBlock = styled.section`
 `;
 
 export const InputBlock = styled.div`
+        margin: 0 auto;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        flex-flow: column nowrap;
-
+        flex-flow: row nowrap;
         color: var(--gray-500);
         font-size: var(--text-md);
         line-height: var(--text-lh-md);
@@ -35,17 +40,20 @@ export const InputBlock = styled.div`
         font-weight: var(--fw-normal);
         margin-top: 0.375rem;
         margin-bottom: 1.5rem;
-        min-width: 343px;
+
+        width: 100%;
+        max-width: 343px;
         min-height: 44px;
-        display: block;
         height: 100%;
+
         padding: 0.6rem 0.8rem;
         border: 1px var(--gray-300) solid;
         border-radius: var(--radii);
         box-shadow: var(--shadow-xs);
 
         input {
-                min-width: 343px;
+                max-width: 343px;
+                width: 100%;
                 background-color: transparent;
                 border: hidden;
                 outline: none;
@@ -58,4 +66,28 @@ export const InputBlock = styled.div`
                 font-family: var(--ff);
                 font-weight: var(--fw-normal);
         }
+
+        i {
+                cursor: pointer;
+                color: var(--gray-500);
+
+                &:hover {
+                        color: var(--gray-700);
+                }
+        }
+`;
+
+export const ErrorBlock = styled.div`
+        margin: 0 auto;
+        max-width: 343px;
+        text-align: center;
+`;
+
+export const TextSeparator = styled.div`
+        margin: 0 auto;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+        align-items: center !important;
+        gap: 0.5rem;
 `;
