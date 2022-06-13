eff303f195e772c2b331636afeb1231678e0fcb2
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-14 01:20:00 +0300

    add basic spliting  & test lazy loading

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,27 @@
-import Home from '../pages/Home';
-import LogIn from '../pages/Authorization/LogIn';
-import SignUp from '../pages/Authorization/SignUp';
-import NotFound from '../pages/NotFound';
-import UserProfile from '../app/pages/UserProfile';
-import RequireAuth from '../context/RequireAuth';
-import Dashboard from '../app/pages/Dashboard';
-import Pricing from '../pages/Pricing';
-import ForgotPass from '../pages/Authorization/ForgotPass';
+import loadable, { lazy } from '@loadable/component';
+import { Suspense } from 'react';
+import { SpinnerXl } from '../styles/UI/Spinners';
+
+const LogIn = loadable(() => import('../pages/Authorization/LogIn'));
+const NotFound = loadable(() => import('../pages/NotFound'));
+const SignUp = loadable(() => import('../pages/Authorization/SignUp'));
+const UserProfile = loadable(() => import('../app/pages/UserProfile'));
+const RequireAuth = loadable(() => import('../context/RequireAuth'));
+const Dashboard = loadable(() => import('../app/pages/Dashboard'));
+const Pricing = loadable(() => import('../pages/Pricing'));
+const ForgotPass = loadable(() => import('../pages/Authorization/ForgotPass'));
+const Home = lazy(() => import('../pages/Home'));
 
 export const publicRoutes = [
-        { index: true, element: <Home />, replace: false },
+        {
+                index: true,
+                element: (
+                        <Suspense fallback={<SpinnerXl />}>
+                                <Home />
+                        </Suspense>
+                ),
+                replace: false,
+        },
         { index: false, path: '/login', element: <LogIn />, replace: false },
         { index: false, path: '/signup', element: <SignUp />, replace: false },
         { index: false, path: '*', element: <NotFound />, replace: false },
