62b5b5b65b2517fc3ad68c4f0237914d2f4e99dc
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-18 13:55:26 +0300

    start fix css issues in safari

--- src/components/Features/FeaturesImg.jsx
+++ src/components/Features/FeaturesImg.jsx
@@ -6,8 +6,13 @@ export const SectionImg = styled(HeroImg)`
         outline: 4px solid var(--gray-100);
         box-shadow: var(--shadow-2xl);
         border-radius: 1rem;
+        -webkit-border-radius: 1rem;
+        -moz-border-radius: 1rem;
+        -khtml-border-radius: 1rem;
         width: 100%;
         object-fit: scale-down;
+        /* -webkit-transform: translateZ(0);
+        -webkit-mask-image: -webkit-radial-gradient(circle, white 100%, black 100%); */
         /* object-position: -20% 0; */
 `;
 
