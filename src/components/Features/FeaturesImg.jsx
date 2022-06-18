830648e09a0418c90c6aefc0db7cd6fd8d68a458
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-18 14:02:10 +0300

    Revert "didn't work"
    This reverts commit a6c8ab9adef9425218ad3af62437ceb5801cfaa4.

--- src/components/Features/FeaturesImg.jsx
+++ src/components/Features/FeaturesImg.jsx
@@ -3,25 +3,17 @@ import { motion } from 'framer-motion';
 import { HeroImg } from '../Hero/Hero.styled';
 
 export const SectionImg = styled(HeroImg)`
+        outline: 4px solid var(--gray-100);
+        box-shadow: var(--shadow-2xl);
+        border-radius: 1rem;
         -webkit-border-radius: 1rem;
         -moz-border-radius: 1rem;
-        border-radius: 1rem;
         -khtml-border-radius: 1rem;
-        vertical-align: middle;
-
+        width: 100%;
+        object-fit: scale-down;
         /* -webkit-transform: translateZ(0);
         -webkit-mask-image: -webkit-radial-gradient(circle, white 100%, black 100%); */
         /* object-position: -20% 0; */
-`;
-
-const ImgWrapper = styled.div`
-        display: inline-block;
-        -webkit-border-radius: 1rem;
-        -moz-border-radius: 1rem;
-        border-radius: 1rem;
-        -khtml-border-radius: 1rem;
-        box-shadow: var(--shadow-2xl);
-        outline: 4px solid var(--gray-100);
 `;
 
 const imgAnimation = {
@@ -37,9 +29,15 @@ const imgAnimation = {
 };
 
 const FeaturesImg = ({ img, alt }) => (
-        <ImgWrapper as={motion.img} initial="hidden" whileInView="visible" variants={imgAnimation}>
-                <SectionImg src={img} alt={alt} loading="lazy" />
-        </ImgWrapper>
+        <SectionImg
+                as={motion.img}
+                initial="hidden"
+                whileInView="visible"
+                variants={imgAnimation}
+                src={img}
+                alt={alt}
+                loading="lazy"
+        />
 );
 
 export default FeaturesImg;
