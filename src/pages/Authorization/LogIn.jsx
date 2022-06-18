55b2efa8eb83a748a900ea45e8595fa4c39d27e9
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-18 18:59:32 +0300

    start fixing

--- src/pages/Authorization/LogIn.jsx
+++ src/pages/Authorization/LogIn.jsx
@@ -47,6 +47,7 @@ const Login = () => {
 
         const googleSignIn = () => {
                 singUpWithGoogle();
+                if (currentUser) navigate('/dashboard');
         };
 
         const toggleVisible = () => {
