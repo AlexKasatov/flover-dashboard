a6c8ab9adef9425218ad3af62437ceb5801cfaa4
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-18 14:01:57 +0300

    didn't work

--- src/components/Features/FeaturesImg.jsx
+++ src/components/Features/FeaturesImg.jsx
@@ -3,17 +3,25 @@ import { motion } from 'framer-motion';
 import { HeroImg } from '../Hero/Hero.styled';
 
 export const SectionImg = styled(HeroImg)`
-        outline: 4px solid var(--gray-100);
-        box-shadow: var(--shadow-2xl);
-        border-radius: 1rem;
         -webkit-border-radius: 1rem;
         -moz-border-radius: 1rem;
+        border-radius: 1rem;
         -khtml-border-radius: 1rem;
-        width: 100%;
-        object-fit: scale-down;
+        vertical-align: middle;
+
         /* -webkit-transform: translateZ(0);
         -webkit-mask-image: -webkit-radial-gradient(circle, white 100%, black 100%); */
         /* object-position: -20% 0; */
+`;
+
+const ImgWrapper = styled.div`
+        display: inline-block;
+        -webkit-border-radius: 1rem;
+        -moz-border-radius: 1rem;
+        border-radius: 1rem;
+        -khtml-border-radius: 1rem;
+        box-shadow: var(--shadow-2xl);
+        outline: 4px solid var(--gray-100);
 `;
 
 const imgAnimation = {
@@ -29,15 +37,9 @@ const imgAnimation = {
 };
 
 const FeaturesImg = ({ img, alt }) => (
-        <SectionImg
-                as={motion.img}
-                initial="hidden"
-                whileInView="visible"
-                variants={imgAnimation}
-                src={img}
-                alt={alt}
-                loading="lazy"
-        />
+        <ImgWrapper as={motion.img} initial="hidden" whileInView="visible" variants={imgAnimation}>
+                <SectionImg src={img} alt={alt} loading="lazy" />
+        </ImgWrapper>
 );
 
 export default FeaturesImg;
